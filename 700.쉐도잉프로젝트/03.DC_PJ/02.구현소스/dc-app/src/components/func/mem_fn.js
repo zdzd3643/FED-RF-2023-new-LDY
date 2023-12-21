f19c64c9c74151abd4fcf74ba0ff54fbbd682840
zdzd3643 <zdzd3643@gmail.com>
2023-12-21 18:19:38 +0900

    ff

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/func/mem_fn.js
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/func/mem_fn.js
@@ -3,36 +3,36 @@
 
 // [ 로컬쓰 클리어 ] /////////
 const clearData = () => {
-  localStorage.clear();
-  console.log("로컬쓰 클리어!");
+    localStorage.clear();
+    console.log("로컬쓰 클리어!");
 }; /////////// clearData //////////////
 
 // [ 로컬쓰 초기체크셋팅! ] ////////////
 const initData = () => {
-  // 만약 로컬스 "mem-data"가 null이면 만들어준다!
-  if (localStorage.getItem("mem-data") === null) {
-      localStorage.setItem(
-          "mem-data",
-          `
-                  [
-                      {
-                          "idx": "1",
-                          "uid":"admin",
-                          "pwd":"1111",
-                          "unm":"Administrator",
-                          "eml":"admin@dc.com"
-                      },
-                      {
-                          "idx": "2",
-                          "uid":"tomtom",
-                          "pwd":"1111",
-                          "unm":"Tom",
-                          "eml":"tom@gmail.com"
-                      }
-                  ]
-              `
-      );
-  }
+    // 만약 로컬스 "mem-data"가 null이면 만들어준다!
+    if (localStorage.getItem("mem-data") === null) {
+        localStorage.setItem(
+            "mem-data",
+            `
+                    [
+                        {
+                            "idx": "1",
+                            "uid":"admin",
+                            "pwd":"1111",
+                            "unm":"Administrator",
+                            "eml":"admin@dc.com"
+                        },
+                        {
+                            "idx": "2",
+                            "uid":"tomtom",
+                            "pwd":"1111",
+                            "unm":"Tom",
+                            "eml":"tom@gmail.com"
+                        }
+                    ]
+                `
+        );
+    }
 }; ///////////// initData /////////////////
 
 export { clearData, initData };
